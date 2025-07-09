ed35a5bee292d963b8082ea4726bec003c2f3392
AGANETHED <astcvetkov1@chsu.ru>
2025-07-10 00:13:32 +0300

    Bug fixes

--- src/main/java/ru/alfabeton/employee/entity/Employee.java
+++ src/main/java/ru/alfabeton/employee/entity/Employee.java
@@ -1,13 +1,12 @@
 package ru.alfabeton.employee.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "employees")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
--- src/main/java/ru/alfabeton/employee/mapper/EmployeeMapper.java
+++ src/main/java/ru/alfabeton/employee/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package ru.alfabeton.employee.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import ru.alfabeton.employee.dto.EmployeeDto;
 import ru.alfabeton.employee.entity.Employee;
@@ -9,7 +10,9 @@ import ru.alfabeton.employee.entity.Employee;
 public interface EmployeeMapper {
     EmployeeDto toDto(Employee employee);
 
+    @Mapping(target = "id", ignore = true)
     Employee toEntity(EmployeeDto employeeDto);
 
+    @Mapping(target = "id", ignore = true)
     void updateFromDto(EmployeeDto employeeDto, @MappingTarget Employee employee);
 }
